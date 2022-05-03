1c0af22cbf9f0a0eec914cb8233f83407f3f0316
devYeonjun <alcls0026@gmail.com>
2022-05-03 14:15:44 +1200

    Edit Own youtube API

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,6 @@ import useVideos from "../hooks/useVideos";
 
 const App = () => {
 	const [selectedVideo, setSelectedVideo] = useState(null);
-
 	const [videos, search] = useVideos("React jsx");
 
 	useEffect(() => {
